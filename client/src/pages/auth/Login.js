21f3e336e5188bf03cca506707d1317e8f8868ab
John Neil Castillo <jnhcastillo78@gmail.com>
2021-08-31 20:58:08 +0800

    Add auth check server side using firebase

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -5,6 +5,19 @@ import { toast } from "react-toastify";
 import { Button } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
+const createOrUpdateUser = async (authToken) => {
+  return await axios.post(
+    `${process.env.REACT_APP_API}/create-or-update-user`,
+    {},
+    {
+      headers: {
+        authToken,
+      },
+    }
+  );
+};
 
 const Login = ({ history }) => {
   const [email, setEmail] = useState("xiche78@gmail.com");
@@ -33,11 +46,16 @@ const Login = ({ history }) => {
       const { user } = result;
       const idTokenResult = await user.getIdTokenResult();
 
-      dispatch({
-        type: "LOGGED_IN_USER",
-        payload: { email: user.email, token: idTokenResult.token },
-      });
-      history.push("/");
+      createOrUpdateUser(idTokenResult.token)
+        .then((res) => console.log("Create or update response", res))
+        .catch();
+
+      // dispatch({
+      //   type: "LOGGED_IN_USER",
+      //   payload: { email: user.email, token: idTokenResult.token },
+      // });
+
+      // history.push("/");
     } catch (error) {
       console.log(error);
       toast.error(error.message);
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,9 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-// destructure controllers
+// middlewares
+const { authCheck } = require("../middlewares/auth");
+
+// destructure controllers due to having many controllers
 const { createOrUpdateUser } = require("../controllers/auth");
 
-router.get("/create-or-update-user", createOrUpdateUser);
+router.post("/create-or-update-user", authCheck, createOrUpdateUser);
 
 module.exports = router;
