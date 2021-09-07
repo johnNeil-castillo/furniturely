e19996e3d7467dd0edd69139ad18d59b038827ec
John Neil Castillo <jnhcastillo78@gmail.com>
2021-09-07 16:05:20 +0800

    Add actual test charge on cart

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -5,10 +5,18 @@ const Coupon = require("../models/coupon");
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 exports.createPaymentIntent = async (req, res) => {
+  const user = await User.findOne({ email: req.user.email }).exec();
+
+  const { cartTotal } = await Cart.findOne({ orderdBy: user._id }).exec();
+
+  console.log("CART TOTAL CHARGED", cartTotal);
+
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 100,
+    amount: cartTotal * 100,
     currency: "usd",
   });
 
-  res.send({ clientSecret: paymentIntent.client_secret });
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
 };
