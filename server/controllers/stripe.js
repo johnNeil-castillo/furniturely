9276bce771f89603bc367625723366fec8293836
John Neil Castillo <jnhcastillo78@gmail.com>
2021-09-07 15:07:48 +0800

    create stripe backend

---
+++ server/controllers/stripe.js
@@ -0,0 +1,14 @@
+const User = require("../models/user");
+const Cart = require("../models/cart");
+const Product = require("../models/product");
+const Coupon = require("../models/coupon");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+exports.createPaymentIntent = async (req, res) => {
+  const paymentIntent = await stripe.paymentIntent.create({
+    amount: 100,
+    currency: "usd",
+  });
+
+  res.send({ clientSecret: paymentIntent.client_secret });
+};
