fe627ce09d56eebb9f8ffc0147664e0c90dbed11
John Neil Castillo <jnhcastillo78@gmail.com>
2021-09-07 15:52:11 +0800

    Add successful payment feature in stripe

--- client/src/components/StripeCheckout.js
+++ client/src/components/StripeCheckout.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import { useSelector, useDispatch } from "react-redux";
 import { createPaymentIntent } from "../functions/stripe";
+import { Link } from "react-router-dom";
 
 const StripeCheckout = ({ history }) => {
   const dispatch = useDispatch();
@@ -43,14 +44,37 @@ const StripeCheckout = ({ history }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: { name: e.target.name.value },
+      },
+    });
+
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`);
+      setProcessing(false);
+    } else {
+      console.log(JSON.stringify(payload, null, 4));
+      setError(null);
+      setProcessing(false);
+      setSucceeded(true);
+    }
   };
 
   const handleChange = async (e) => {
-    setDisabled(e.error ? e.error.message : "");
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : "");
   };
 
   return (
     <>
+      <p className={succeeded ? "result-message" : "result-message hidden"}>
+        Payment Successful{" "}
+        <Link to="/user/history">See it in your purchase history</Link>
+      </p>
       <form id="payment-form" className="stripe-form" onSubmit={handleSubmit}>
         <CardElement
           id="card-element"
@@ -66,6 +90,13 @@ const StripeCheckout = ({ history }) => {
           </span>
         </button>
       </form>
+      <br />
+
+      {error && (
+        <div className="card-error" role="alert">
+          {error}
+        </div>
+      )}
     </>
   );
 };
